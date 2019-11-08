d718a3ee20958854bb0c1dd819b30edbaf969135
chuheng wu <wuchuheng@163.com>
2019-11-08 15:29:33 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -14,7 +14,8 @@ use Wuchuheng\Weather\Exceptions\HttpException;
 class Weather
 {
     protected $key;
-    protected $_guzzleOptions= [];
+
+    protected $_guzzleOptions = [];
 
     public function __construct($key)
     {
@@ -32,35 +33,33 @@ class Weather
     }
 
     /**
-     *  获取天气
+     *  获取天气.
      *
      *  @city   mix    城市名 / 高德地址位置 adcode，比如：“深圳” 或者（adcode：440300）
-     *  @type   string 返回内容类型：base: 返回实况天气 / all: 返回预报天气；
-     *  @format string 返回的数据格式json|xml
      *
+     *  @var   string 返回内容类型：base: 返回实况天气 / all: 返回预报天气；
+     *  @format string 返回的数据格式json|xml
      */
-
-
-
-
     public function getWeather($city, string $type = 'live', string $format = 'json')
     {
         $types = [
-            'live'    => 'base',
-            'forcast' => 'all'
+            'live' => 'base',
+            'forcast' => 'all',
         ];
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
-        if (!in_array(strtolower($format), ['json', 'xml']))
-            throw new InvalidArgumentException('Invalid response format: ' . $format);
-        if (!in_array($type, array_keys($types))) 
-           throw new InvalidArgumentException('Invalid type value(base/all) ' . $type);
-        
-       $query = array_filter([
+        if (!in_array(strtolower($format), ['json', 'xml'])) {
+            throw new InvalidArgumentException('Invalid response format: '.$format);
+        }
+        if (!in_array($type, array_keys($types))) {
+            throw new InvalidArgumentException('Invalid type value(base/all) '.$type);
+        }
+        $query = array_filter([
            'key' => $this->key,
            'city' => $city,
            'output' => $format,
-           'extensions' =>  $types[$type],
+           'extensions' => $types[$type],
         ]);
+
         try {
             $response = $this->getHttpClient()
                 ->get($url, [
@@ -68,10 +67,10 @@ class Weather
                 ])
                 ->getBody()
                 ->getContents();
-            return "json" === $format ? json_decode($response, true) : $response;
-        } catch(\Exception $e) {
+
+            return 'json' === $format ? json_decode($response, true) : $response;
+        } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
 }
-
